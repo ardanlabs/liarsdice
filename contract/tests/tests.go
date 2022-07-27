f2be268adeec37f1ab6db313113e25c8b8189afe
gerep <daniel.gerep@protonmail.com>
2022-07-27 10:09:33 -0300

    add keystore test folder

--- contract/tests/tests.go
+++ contract/tests/tests.go
@@ -8,15 +8,15 @@ import (
 )
 
 const (
-	PrimaryKeyPath    = "UTC--2022-05-12T14-47-50.112225000Z--6327a38415c53ffb36c11db55ea74cc9cb4976fd"
+	PrimaryKeyPath    = "keystore/UTC--2022-05-12T14-47-50.112225000Z--6327a38415c53ffb36c11db55ea74cc9cb4976fd"
 	PrimaryPassPhrase = "123"
 
 	Player1Address    = "0x0070742ff6003c3e809e78d524f0fe5dcc5ba7f7"
-	Player1KeyPath    = "UTC--2022-05-13T16-59-42.277071000Z--0070742ff6003c3e809e78d524f0fe5dcc5ba7f7"
+	Player1KeyPath    = "keystore/UTC--2022-05-13T16-59-42.277071000Z--0070742ff6003c3e809e78d524f0fe5dcc5ba7f7"
 	Player1PassPhrase = "123"
 
 	Player2Address    = "0x8e113078adf6888b7ba84967f299f29aece24c55"
-	Player2KeyPath    = "UTC--2022-05-13T16-57-20.203544000Z--8e113078adf6888b7ba84967f299f29aece24c55"
+	Player2KeyPath    = "keystore/UTC--2022-05-13T16-57-20.203544000Z--8e113078adf6888b7ba84967f299f29aece24c55"
 	Player2PassPhrase = "123"
 )
 
