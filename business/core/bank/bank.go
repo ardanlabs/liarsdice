91a5d8583ef4e9d661a051a200e962ce1faba772
William Kennedy <bill@ardanlabs.com>
2022-08-09 08:43:30 -0400

    bank changed

--- business/core/bank/bank.go
+++ business/core/bank/bank.go
@@ -14,26 +14,26 @@ type Bank struct {
 }
 
 // NewBank returns a new bank with the ability to manage the game money.
-func NewBank(ctx context.Context, network string, keyPath string, passPhrase string) (Bank, error) {
+func NewBank(ctx context.Context, network string, keyPath string, passPhrase string) (*Bank, error) {
 	client, err := smart.Connect(ctx, network, keyPath, passPhrase)
 	if err != nil {
-		return Bank{}, err
+		return nil, err
 	}
 
 	bank := Bank{
 		client: client,
 	}
 
-	return bank, nil
+	return &bank, nil
 }
 
 // PlayerBalance will return the specified player's balance.
-func (b Bank) PlayerBalance(address string) (*big.Int, error) {
+func (b *Bank) PlayerBalance(address string) (*big.Int, error) {
 	return nil, nil
 }
 
 // Reconcile will apply with ante to the winner and losers and provide the
 // house the game fee.
-func (b Bank) Reconcile(winner string, losers []string, ante uint, gameFee uint) error {
+func (b *Bank) Reconcile(winner string, losers []string, ante uint, gameFee uint) error {
 	return nil
 }
