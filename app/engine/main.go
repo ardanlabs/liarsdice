55c77669a032671e914c68c6c73a5b6f7a2a5e41
William Kennedy <bill@ardanlabs.com>
2022-08-10 08:13:47 -0400

    refactor bank api

--- app/engine/main.go
+++ app/engine/main.go
@@ -161,8 +161,16 @@ func run(log *zap.SugaredLogger) error {
 	// =========================================================================
 	// Start Game and Bank Services
 
-	ctx := context.Background()
-	bank, err := bank.NewBank(ctx, smart.NetworkLocalhost, smart.PrimaryKeyPath, smart.PrimaryPassPhrase)
+	data, err := os.ReadFile("zarf/contract/id.env")
+	if err != nil {
+		return fmt.Errorf("readfile: %w", err)
+	}
+	contractID := string(data)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	bank, err := bank.NewBank(ctx, smart.NetworkLocalhost, smart.PrimaryKeyPath, smart.PrimaryPassPhrase, contractID)
 	if err != nil {
 		return fmt.Errorf("connecting to db: %w", err)
 	}
--- business/core/bank/bank.go
+++ business/core/bank/bank.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/big"
-	"os"
 
 	"github.com/ardanlabs/liarsdice/contract/sol/go/contract"
 	"github.com/ardanlabs/liarsdice/foundation/smartcontract/smart"
@@ -14,18 +13,25 @@ import (
 // Bank represents a bank that allows for the reconciling of a game and
 // information about player balances.
 type Bank struct {
-	client *smart.Client
+	client   *smart.Client
+	contract *contract.Contract
 }
 
 // NewBank returns a new bank with the ability to manage the game money.
-func NewBank(ctx context.Context, network string, keyPath string, passPhrase string) (*Bank, error) {
+func NewBank(ctx context.Context, network string, keyPath string, passPhrase string, contractID string) (*Bank, error) {
 	client, err := smart.Connect(ctx, network, keyPath, passPhrase)
 	if err != nil {
 		return nil, err
 	}
 
+	contract, err := contract.NewContract(common.HexToAddress(contractID), client.ContractBackend())
+	if err != nil {
+		return nil, fmt.Errorf("NewContract: %w", err)
+	}
+
 	bank := Bank{
-		client: client,
+		client:   client,
+		contract: contract,
 	}
 
 	return &bank, nil
@@ -33,11 +39,6 @@ func NewBank(ctx context.Context, network string, keyPath string, passPhrase str
 
 // PlayerBalance will return the specified address balance.
 func (b *Bank) PlayerBalance(ctx context.Context, address string) (*big.Int, error) {
-	contract, err := newContract(b.client)
-	if err != nil {
-		return nil, err
-	}
-
 	tranOpts, err := b.client.NewCallOpts(ctx)
 	if err != nil {
 		return nil, err
@@ -45,27 +46,11 @@ func (b *Bank) PlayerBalance(ctx context.Context, address string) (*big.Int, err
 
 	player := common.HexToAddress(address)
 
-	return contract.PlayerBalance(tranOpts, player)
+	return b.contract.PlayerBalance(tranOpts, player)
 }
 
 // Reconcile will apply with ante to the winner and losers and provide the
 // house the game fee.
 func (b *Bank) Reconcile(ctx context.Context, winner string, losers []string, ante uint, gameFee uint) error {
 	return nil
-}
-
-// newContract constructs a SimpleCoin contract.
-func newContract(client *smart.Client) (*contract.Contract, error) {
-	data, err := os.ReadFile("zarf/contract/id.env")
-	if err != nil {
-		return nil, fmt.Errorf("readfile: %w", err)
-	}
-	contractID := string(data)
-
-	contract, err := contract.NewContract(common.HexToAddress(contractID), client.ContractBackend())
-	if err != nil {
-		return nil, fmt.Errorf("NewContract: %w", err)
-	}
-
-	return contract, nil
 }
