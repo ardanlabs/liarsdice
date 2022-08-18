3216fe92966191a2197a0ba2885b11f0fd4f7741
Bruno Belucci <bruno.belucci@ardanlabs.com>
2022-08-18 19:02:23 -0300

    dynamic contract address in frontend

--- app/client/assets/frontend/src/contracts/index.ts
+++ app/client/assets/frontend/src/contracts/index.ts
@@ -1,4 +1,9 @@
-// import axios from "axios"
+import axios from 'axios'
+let fetchedAddress = '0x5c63E35252A807815E1dF566b0eF04DeB1372464'
 
-// axios.get(local)
-export const contractAddress = '0x5c63E35252A807815E1dF566b0eF04DeB1372464'
+axios
+  .get('id.env')
+  .then((res) => (fetchedAddress = res.data))
+  .catch((err) => console.log(err))
+
+export const contractAddress = fetchedAddress
