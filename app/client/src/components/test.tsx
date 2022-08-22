ed66156bd765156b2b109b670f11529e5d544b72
Bruno Belucci <bruno.belucci@ardanlabs.com>
2022-08-22 11:43:25 -0300

    added test call

--- app/client/src/components/test.tsx
+++ app/client/src/components/test.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { utils } from 'ethers'
 import { useEthers } from '@usedapp/core'
+import axios, { AxiosError } from 'axios'
 
 const Test = () => {
   const { library } = useEthers()
 
-  const signMessage = (message: string) => {
+  const signMessage = () => {
+    var doc = { name: 'bill', status: 'ok' }
+
     const signer = library?.getSigner()
+
     // Marshal the transaction to a string and convert the string to bytes.
-    const marshal = JSON.stringify(message)
+    const marshal = JSON.stringify(doc)
     const marshalBytes = utils.toUtf8Bytes(marshal)
+
     // Hash the transaction data into a 32 byte array. This will provide
     // a data length consistency with all transactions.
     const txHash = utils.keccak256(marshalBytes)
@@ -20,11 +25,19 @@ const Test = () => {
 
     signer?.signMessage(bytes).then((response: any) => {
       console.log(response)
+      const data = { ...doc, sig: response }
+      axios
+        .post('http://localhost:3000/v1/game/test', data)
+        .then((response) => {
+          console.log(response, 'in')
+        })
+        .catch((error: AxiosError) => {
+          console.log(error)
+        })
     })
   }
-  const message = JSON.stringify({ name: 'bill', status: 'ok' })
 
-  return <button onClick={() => signMessage(message)}>Test signing</button>
+  return <button onClick={() => signMessage()}>Test signing</button>
 }
 
 export default Test
