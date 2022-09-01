8aec0c4c516296dc04768644b0cf0de5a8f7f461
William Kennedy <bill@ardanlabs.com>
2022-09-01 14:51:29 -0400

    basic working input functions

--- app/cli/liars/main.go
+++ app/cli/liars/main.go
@@ -42,9 +42,23 @@ func run() error {
 					close(quit)
 					return
 				case tcell.KeyEnter:
-					message(s, style, "didn't provide proper bet")
+					enter(s, style)
 				case tcell.KeyCtrlL:
 					s.Sync()
+				case tcell.KeyRune:
+					r := ev.Rune()
+					switch {
+					case r == rune('d'):
+						enterDeposit(s, style)
+					case r == rune('b'):
+						enterBet(s, style)
+					case (r >= rune('0') && r <= rune('9')) || r == rune('.'):
+						key(s, style, ev.Rune())
+					case r == rune('-'):
+						minus(s, style)
+					default:
+						s.Beep()
+					}
 				}
 			}
 		}
@@ -53,6 +67,133 @@ func run() error {
 	<-quit
 
 	return nil
+}
+
+var bets []rune
+var deposit []rune
+var cursor string
+
+func enter(s tcell.Screen, style tcell.Style) {
+	switch cursor {
+	case "bet":
+		enterBet(s, style)
+
+	case "deposit":
+		enterDeposit(s, style)
+
+	default:
+		s.Beep()
+	}
+}
+
+func minus(s tcell.Screen, style tcell.Style) {
+	switch cursor {
+	case "bet":
+		subBet(s, style)
+
+	case "deposit":
+		subDeposit(s, style)
+
+	default:
+		s.Beep()
+	}
+}
+
+func key(s tcell.Screen, style tcell.Style, r rune) {
+	switch cursor {
+	case "bet":
+		if r >= rune('1') && r <= rune('6') {
+			addBet(s, style, r)
+			return
+		}
+		s.Beep()
+
+	case "deposit":
+		addDeposit(s, style, r)
+
+	default:
+		s.Beep()
+	}
+}
+
+func enterDeposit(s tcell.Screen, style tcell.Style) {
+	cursor = "deposit"
+	deposit = []rune{}
+
+	s.ShowCursor(12, 19)
+	s.SetContent(12, 19, ' ', nil, style)
+	s.SetCursorStyle(tcell.CursorStyleBlinkingBlock)
+
+	emitStr(s, 12, 19, style, "                             ")
+	emitStr(s, 13, 10, style, "                             ")
+}
+
+func addDeposit(s tcell.Screen, style tcell.Style, r rune) {
+	if cursor != "deposit" {
+		s.Beep()
+		return
+	}
+
+	x := 11
+	deposit = append(deposit, r)
+	x += len(deposit)
+
+	s.ShowCursor(x+1, 19)
+	emitStr(s, x, 19, style, string(r))
+}
+
+func subDeposit(s tcell.Screen, style tcell.Style) {
+	if cursor != "deposit" || len(deposit) == 0 {
+		s.Beep()
+		return
+	}
+
+	x := 11
+	x += len(deposit)
+	deposit = deposit[:len(deposit)-1]
+
+	s.ShowCursor(x, 19)
+	emitStr(s, x, 19, style, " ")
+}
+
+func enterBet(s tcell.Screen, style tcell.Style) {
+	cursor = "bet"
+	bets = []rune{}
+
+	s.ShowCursor(13, 10)
+	s.SetContent(13, 10, ' ', nil, style)
+	s.SetCursorStyle(tcell.CursorStyleBlinkingBlock)
+
+	emitStr(s, 12, 19, style, "                             ")
+	emitStr(s, 13, 10, style, "                             ")
+}
+
+func subBet(s tcell.Screen, style tcell.Style) {
+	if cursor != "bet" || len(bets) == 0 {
+		s.Beep()
+		return
+	}
+
+	x := 12
+	x += len(bets)
+	bets = bets[:len(bets)-1]
+
+	s.ShowCursor(x, 10)
+	emitStr(s, x, 10, style, " ")
+}
+
+func addBet(s tcell.Screen, style tcell.Style, r rune) {
+	if cursor != "bet" || (len(bets) > 0 && bets[0] != r) {
+		s.Beep()
+		return
+	}
+
+	x := 12
+	bets = append(bets, r)
+	x += len(bets)
+
+	s.ShowCursor(x+1, 10)
+	emitStr(s, x, 10, style, string(r))
 }
 
 func makeBoard(s tcell.Screen, style tcell.Style) {
@@ -95,15 +236,14 @@ func makeBoard(s tcell.Screen, style tcell.Style) {
 	emitStr(s, 35, 8, style, "Ante:  $5 USD")
 	emitStr(s, 35, 9, style, "Pot : $15 USD")
 
-	emitStr(s, 3, 10, style, "My Bet: ___ ______")
+	emitStr(s, 3, 10, style, "My Bet :>")
+	emitStr(s, 1, 19, style, "Deposit :>")
 
 	emitStr(s, 65, 2, style, "<1-6>   : set/increment bet")
 	emitStr(s, 65, 3, style, "<minus> : decrement bet")
-	emitStr(s, 65, 4, style, "<0>     : reset bet")
-	emitStr(s, 65, 5, style, "<enter> : place bet")
-	emitStr(s, 65, 6, style, "<l>     : call liar")
-
-	s.Show()
+	emitStr(s, 65, 4, style, "<b>     : place bet")
+	emitStr(s, 65, 5, style, "<l>     : call liar")
+	emitStr(s, 65, 6, style, "<d>     : deposit funds")
 }
 
 func message(s tcell.Screen, style tcell.Style, message string) {
@@ -123,4 +263,5 @@ func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
 		s.SetContent(x, y, c, comb, style)
 		x += w
 	}
+	s.Show()
 }
