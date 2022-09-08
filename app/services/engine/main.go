dfc66423cde2dbd26be24f2c56222f94b91cab17
Bruno Belucci <bruno.belucci@ardanlabs.com>
2022-09-08 10:15:17 -0300

    todo list updated

--- app/services/engine/main.go
+++ app/services/engine/main.go
@@ -34,9 +34,9 @@ import (
 	Can I send money to a contract address after contract is in place?
 
 	-- Browser UI
-	Convert everything into functions.
 	Make the network change work.
 	Add subtle animations to some game elements (For Ex. Cups).
+	Look into the double websocket connection and then handle the reconnection.
 	Change players inditifiers from addresses to names.
 */
 
