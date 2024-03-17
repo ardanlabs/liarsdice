a2f55e7ba4627c64b0c95cdfcc75b30799481b0a
William Kennedy <bill@ardanlabs.com>
2024-03-17 13:02:50 -0400

    more code reorg

--- app/services/ui/website/scripts/engine.js
+++ app/services/ui/website/scripts/engine.js
@@ -1,3 +1,36 @@
+async function config(url) {
+    try {
+        const result = await $.ajax({
+            type: "get",
+            url: `${url}/v1/game/config`
+        });
+
+        return [result, null];
+    }
+    
+    catch (e) {
+        return [null, e.responseJSON];
+    }
+}
+
+async function connectGameEngine(url, address, chainId, dateTime, sigature) {
+    const data = `{"address":"${address}","chainId":${chainId},"dateTime":"${dateTime}","sig":"${sigature}"}`;
+
+    try {
+        const token = await $.ajax({
+            type: "post",
+            url: `${url}/v1/game/connect`,
+            data: data
+        });
+
+        return [token, null];
+    }
+
+    catch (e) {
+        return [null, e.responseJSON];
+    }
+}
+
 async function getGameTables(url, token) {
     try {
         const tables = await $.ajax({
